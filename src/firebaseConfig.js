dc5571ef098c69ef6e9f6053f794876a1579e1fe
SachinPawar11794 <151117622+SachinPawar11794@users.noreply.github.com>
2025-12-26 12:13:10 +0530

    Add Firebase config and initialization

---
+++ src/firebaseConfig.js
@@ -0,0 +1,12 @@
+// Firebase config for my-project-40013
+export const firebaseConfig = {
+  apiKey: "AIzaSyB-Rttv8XMW0nOgoyy8IVZOKFnRzBCznog",
+  authDomain: "my-project-40013.firebaseapp.com",
+  projectId: "my-project-40013",
+  storageBucket: "my-project-40013.firebasestorage.app",
+  messagingSenderId: "169633813068",
+  appId: "1:169633813068:web:9377f076c379aa89e45ae9",
+  measurementId: "G-5PCDJPMENQ"
+};
+
+
